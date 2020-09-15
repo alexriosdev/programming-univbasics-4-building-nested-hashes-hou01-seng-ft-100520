d161798a6265f422bcab758e5d10d55be53386bb
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-15 08:24:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,7 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
   
-  {railroads: {}}
+  {:railroads => {}}
 
 end
 
@@ -13,10 +13,10 @@ def monopoly_with_second_tier
   # complexity.
   
   {
-    railroads: {
-      pieces: 4,
-      rent_in_dollars: {},
-      names: {}
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {},
+      :names => {}
     }
   }
 
